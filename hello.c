81afe36e58fbde7dc209bfc5155696430c195154
yuviiikachaudhary <yuviiikachaudhary20@gmail.com>
2026-08-11 23:17:07 +0530

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+int a,b;
+printf("enter the value of a");
+scanf("%d",&a);
+printf (" entre the the value of b");
+scanf("%d",&b);
+printf("sum is :%d", a + b);
+
+return 0;
+}

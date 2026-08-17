35fedc4d9cf2fac4f4c72fd9f4b4626e1f9c3afb
yuviiikachaudhary <yuviiikachaudhary20@gmail.com>
2026-08-17 11:57:35 +0530

    Refactor Day1_2.c for improved input handling

---
+++ Day1_2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+int a, b;
+// Input 1
+scanf("%d %d ", &a , &b);
+printf("sum=%d, diff=%d, product=%d, quotient=%d\n", a+b, a-b, a*b, a/b);
+
+// Input 2
+scanf("%d %d," &a, &b);
+printf("sum=%d, diff=%d, product=%d, quotient=%d",a+b, a-b, a*b, a/b);
+
+return 0;
+}
+
+  

746f515b5196fc5ff918a72f7445190dc2cfd419
yuviiikachaudhary <yuviiikachaudhary20@gmail.com>
2026-08-30 21:12:54 +0530

    Seconds to Hours:Minutes:Seconds Conversion

---
+++ Day_5.2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int totalSeconds, hours, minutes, seconds;
+
+    printf("Enter time in seconds: ");
+    scanf("%d", &totalSeconds);
+
+    hours = totalSeconds / 3600;
+    minutes = (totalSeconds % 3600) / 60;
+    seconds = totalSeconds % 60;
+
+    printf("Time = %02d:%02d:%02d\n", hours, minutes, seconds);
+
+    return 0;
+    }
